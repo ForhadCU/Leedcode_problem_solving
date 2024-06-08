a5d95e3844c1a2ec584cdae2b9245644a8eb75ee
ForhadCU <kashem.tipo@gmail.com>
2024-06-08 22:06:16 +0600

    Two pass hashtable

--- TwoSum.java
+++ TwoSum.java
@@ -1,20 +1,30 @@
+import java.util.HashMap;
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        // Using brute force
-        for (int index = 0; index < nums.length - 1; index++) {
-            for (int j = index + 1; j < nums.length; j++) {
-                if (nums[index] + nums[j] == target) {
-                    // System.out.println("[" + index + "," + j + "]");
-                    return new int[] { index, j };
-                }
+        // Using two pass hashTable
+        HashMap<Integer, Integer> hMap = new HashMap<>();
+
+        // place all the value in hashmap as numbers are key and indices are values
+        for (int i = 0; i < nums.length; i++) {
+            hMap.put(nums[i], i);
+        }
+
+        // generate output
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (hMap.containsKey(complement) && hMap.get(complement) != i) {
+                System.out.println(i + " "+ hMap.get(complement));
+                return new int[] { i, hMap.get(complement), };
             }
         }
+
         return new int[] {};
     }
 
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        int[] inArray = new int[] { 3, 3 };
+        int[] inArray = new int[] { 2, 3, 3 };
         twoSum.twoSum(inArray, 6);
 
     }
